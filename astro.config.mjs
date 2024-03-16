094a34f814a3833ca957ba50470e1fcbc9b00034
Pablo Elgueta <pablo.elguetav@gmail.com>
2024-03-16 00:21:01 -0300

    PEV 16-03-2024: Se configura para desplegar en Netfily

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
